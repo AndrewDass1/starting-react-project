b9a4054d15e0abd73a6e8ba9367ee690ab32069d
Andrew Dass <dassandrew3@gmail.com>
2026-07-22 09:59:56 -0400

    lesson3 work in progress, needs a few more edits but will reach out for help and then commit further changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import './App.css';
 import TodoForm from './TodoForm.jsx';
 import TodoList from './TodoList.jsx';
+import { useState } from 'react';
+
+const todos = [
+    {id: 1, title: "review resources"},
+    {id: 2, title: "take notes"},
+    {id: 3, title: "code out app"},
+  ]
 
 function App() {
 
-  // const todoList = [
-  //   {id: 1, title: "review resources"},
-  //   {id: 2, title: "take notes"},
-  //   {id: 3, title: "code out app"},
-  // ]
+  const [todoList, SetTodoList] = useState(todos);
+  // const [todoList, SetTodoList] = useState(todos.todoList);
+
 
   return (
     <div>
       <h1>My Todo List</h1>
       <TodoForm />
-      <TodoList />
+      <TodoList todoList={todoList} />
     </div>
   );
 }
---
+++ src/TodoListItem.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function TodoListItem(){
+
+    // const [todoList, SetTodoList] = useState(todoList);
+
+    // return ( <ul> {todoList.map(todo => {todo.title})} </ul>);
+
+};
+
+export default TodoListItem();
