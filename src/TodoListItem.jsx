7fcbbea59cd2583c5a7787ba6bb8f1a005cd8d75
Andrew Dass <dassandrew3@gmail.com>
2026-08-07 03:41:58 -0400

    started lesson05 and pushing changes will keep working on it to fix mistakes

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,5 +1,12 @@
 function TodoListItem({todo}) {
-    return <li> { todo.title } </li>;
+    return <li>
+        <input 
+            type="checkbox"
+            checked={todo.isCompleted}
+            onChange={() => onCompleteTodo(todo.id)}
+        />
+        {todo.title} 
+    </li>;
 }
 
 export default TodoListItem;
