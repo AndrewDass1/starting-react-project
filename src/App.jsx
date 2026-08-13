9fdd253d1edd69d81b115e8c8b9a4f8d6cf54b49
Andrew Dass <dassandrew3@gmail.com>
2026-08-13 00:10:58 -0400

    after reviewing class videos, I attempted to do week 5 assignment again

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,21 @@ function App() {
 
   const [todoList, SetTodoList] = useState([]);
 
-  console.log(todoList);
-  console.log(todoList[0]);
-
-  let condition = todoList.length > 0 ? console.log(todoList[0].id) : '';
-  // console.log(condition);
-
   function addTodo(todoTitle){
       let todo = [{id: Date.now(), title: todoTitle, isCompleted: false}]
 
       return SetTodoList( todoList =>[...todo, ...todoList]);
   }
 
-  function completeTodo(id){
-    const [trueToDo, setTrueToDo] = useState('');
 
-    todoList.map( (todo) => id == todo.id ? setTrueToDo({...todo, isCompleted: true}) : setTrueToDo({...todo, isCompleted: false}))
-    
-    return setTrueToDo();
+  function completeTodo(id) {
+    SetTodoList( (getToDoList) => getToDoList.map( (todo) => (todo.id == id) ? {...todo, isCompleted: true}  : todo ) )
   }
 
   return (
     <div>
       <h1>My Todo List</h1>
-      <TodoList todoList={todoList} 
-        onCompleteTodo={completeTodo}
-      />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
       <TodoForm onAddTodo={addTodo} />
     </div>
   );
