65fedf40038b000e335d36f769ceb4a72c27ea9d
Andrew Dass <dassandrew3@gmail.com>
2026-08-13 01:22:50 -0400

    made more changes, resolved syntax issues and edited filteredTodoList

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,10 +1,12 @@
 import TodoListItem from './TodoListItem.jsx';
 
+import { useState } from 'react';
+
 function TodoList({todoList, onCompleteTodo}) {
+    
+    const filteredTodoList = todoList.filter((todo) => todo.isCompleted == true);
 
-    const filteredTodoList = (todo) => todoList.filter(todo.isCompleted = true)
-
-    return (filteredTodoList.length == 0 ? (<p> Add todo above to get started </p>) : <ul> {todoList.map( (todo) => <TodoListItem key={todo.id} todo={todo} onCompleteTodo={onCompleteTodo}/> ) } </ul> );
+    return (filteredTodoList.length == 0 ? (<p>Add todo above to get started</p>) : <ul> {filteredTodoList.map( (todo) => <TodoListItem key={todo.id} todo={todo} onCompleteTodo={onCompleteTodo}/> ) } </ul> );
 }
 
 export default TodoList;
