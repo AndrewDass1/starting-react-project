0d4a7996be7d2033a280804fff49c33ce4799f33
Andrew Dass <dassandrew3@gmail.com>
2026-08-13 01:46:03 -0400

    fixed todolist.jsx by making changes recommended by Airhub once again

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -4,7 +4,7 @@ function TodoList({todoList, onCompleteTodo}) {
     
     const filteredTodoList = todoList.filter((todo) => todo.isCompleted == false);
 
-    return (filteredTodoList.length == 0 ? (<p>Add todo above to get started</p>) : <ul> {todoList.map( (todo) => <TodoListItem key={todo.id} todo={todo} onCompleteTodo={onCompleteTodo}/> ) } </ul> );
+    return (filteredTodoList.length == 0 ? (<p>Add todo above to get started</p>) : <ul> {filteredTodoList.map( (todo) => <TodoListItem key={todo.id} todo={todo} onCompleteTodo={onCompleteTodo}/> ) } </ul> );
 }
 
 export default TodoList;
