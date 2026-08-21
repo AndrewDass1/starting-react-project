0183c32a0f1b485f2e1fe5b2c3049c228db6bd56
Andrew Dass <dassandrew3@gmail.com>
2026-08-21 10:23:21 -0400

    completed parts 1-6, will add, modify or fix this code if needed

--- src/features/Todos/TodosPage.jsx
+++ src/features/Todos/TodosPage.jsx
@@ -13,6 +13,20 @@ export function TodosPage({token}) {
 
     async function addTodo(todoTitle){
         let todo = [{id: Date.now(), title: todoTitle, isCompleted: false}];
+            
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(todo.title, todo.isCompleted),
+            headers: { 'Content-Type': 'application/json, X-CSRF-TOKEN'},
+            credentials: 'include'
+        };
+        
+        try {
+            const response = await fetch(`http://localhost:3001/api/tasks`, options);
+        } catch (error) {
+            setauthError(`Authentication failed: ${data?.message}`);
+        }
+
 
         return SetTodoList( todoList =>[...todo, ...todoList]);
     }
@@ -20,13 +34,68 @@ export function TodosPage({token}) {
 
     async function completeTodo(id) {
         SetTodoList( (getToDoList) => getToDoList.map( (todo) => (todo.id == id) ? {...todo, isCompleted: !todo.isCompleted}  : todo ) )
+
+        const options = {
+            method: 'PATCH',
+            body: JSON.stringify(todo.isCompleted),
+            headers: { 'Content-Type': 'application/json, X-CSRF-TOKEN'},
+            credentials: 'include'
+        };
+
+        try {
+            const response = await fetch(`http://localhost:3001/api/tasks/${id}`, options);
+        } catch (error) {
+            setauthError(`Authentication failed: ${data?.message}`);
+        }
     }
 
     async function updateTodo(editedTodo){
         const updatedTodos = todoList.map( (todo) => todo.id === editedTodo.id ? {...editedTodo}:todo );
 
         SetTodoList(updatedTodos);
+
+        const options = {
+            method: 'PATCH',
+            body: JSON.stringify(todo.title, todo.isCompleted),
+            headers: { 'Content-Type': 'application/json, X-CSRF-TOKEN'},
+            credentials: 'include'
+        };
+
+        try {
+            const response = await fetch(`http://localhost:3001/api/tasks/${editedTodo.id}`, options);
+        } catch (error) {
+            setauthError(`Authentication failed: ${data?.message}`);
+        }
     }
+
+    useEffect( () => {
+        async function fetchTodos() {
+            setIsTodoListLoading(true);
+
+            try {
+                const params = new URLSearchParams({
+                        limit: 100,
+                    });
+                const response = await fetch(`/api/tasks?${params}`, {
+                        headers: {
+                            'X-CSRF-TOKEN': token,
+                        },
+                    credentials: 'include',
+                });
+            }
+            catch (error) {
+                if (error === 401) {
+                    setError('unauthorized')
+                }
+                else {
+                    setError(`Error:, ${error.name} | ${error.message}`);
+                }
+            }
+            finally {
+                setIsTodoListLoading(false);
+            }
+        }
+    }, []);
 
 
     return (
