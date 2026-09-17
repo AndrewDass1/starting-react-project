4c98a3a711b8cb6e29560b3ba88f1e1b7591a69d
Andrew Dass <dassandrew3@gmail.com>
2026-09-17 00:43:53 -0400

    fixed header.jsx and other syntax issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router';
-
 import Header from './shared/Header.jsx';
 
 import HomePage from './pages/HomePage.jsx';
@@ -10,40 +9,43 @@ import ProfilePage from './pages/ProfilePage.jsx';
 import NotFoundPage from './pages/NotFoundPage.jsx';
 
 import RequireAuth from './components/RequireAuth.jsx';
+import { AuthProvider } from './contexts/AuthContext.jsx';
 
 import './body.module.css';
 
 function App() {
   return (
-    <div>
-      <Header />
+    <AuthProvider>
+      <div>
+        <Header />
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/login" element={<LoginPage />} />
 
-        <Route
-          path="/todos"
-          element={
-            <RequireAuth>
-              <TodosPage />
-            </RequireAuth>
-          }
-        />
+          <Route
+            path="/todos"
+            element={
+              <RequireAuth>
+                <TodosPage />
+              </RequireAuth>
+            }
+          />
 
-        <Route
-          path="/profile"
-          element={
-            <RequireAuth>
-              <ProfilePage />
-            </RequireAuth>
-          }
-        />
+          <Route
+            path="/profile"
+            element={
+              <RequireAuth>
+                <ProfilePage />
+              </RequireAuth>
+            }
+          />
 
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </div>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 }
 
